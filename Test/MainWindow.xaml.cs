cb212456743e88bcc07a2e9249f6c5cb1ac8cca1
sudokar <egor.tomashov@gmail.com>
2022-09-18 14:44:55 +0300

    Добавьте файлы проекта.

---
+++ Test/MainWindow.xaml.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.CodeDom;
+using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Controls.DataVisualization.Charting;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using Test.Model;
+
+namespace Test
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public void ShowGraph()
+        {
+            var result = new Dictionary<int, int>();
+
+            var s = LB1.Items[0];
+            KeyValuePair<string, List<Users>> sel = (KeyValuePair<string, List<Users>>)s;
+
+            for (int i = 1; i < 31; i++)
+            {
+                result.Add(i, sel.Value[i - 1].Steps);
+            }
+
+            ((LineSeries)mcChart.Series[0]).ItemsSource = result;
+        }
+
+        public void ShowGraph(object s)
+        {
+            var result = new Dictionary<int, int>();
+
+            //s = LB1.SelectedItem;
+            KeyValuePair<string, List<Users>> sel = (KeyValuePair<string, List<Users>>)s;
+
+            for (int i = 1; i < sel.Value.Count; i++)
+            {
+                result.TryAdd(i, sel.Value[i - 1].Steps);
+            }
+
+            ((LineSeries)mcChart.Series[0]).ItemsSource = result;
+        }
+
+
+
+        public MainWindow()
+        {
+            var d = Users.GetUsers();
+            Resources["Users"] = d;
+
+            InitializeComponent();
+        }
+
+        private void Grid_MouseWheel(object sender, MouseWheelEventArgs e)
+        {
+            var s = LB1.SelectedItem;
+            MessageBox.Show(s.ToString());
+        }
+
+        private void LineSeries_Loaded(object sender, RoutedEventArgs e)
+        {
+            ShowGraph();
+        }
+
+        private void LB1_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            ShowGraph(LB1.SelectedItem);
+        }
+    }
+}
---
+++ Test/Model/Users.cs
@@ -0,0 +1,100 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace Test.Model
+{
+
+
+
+
+    public class Users
+    {
+
+
+
+        public int Rank { get; set; }
+        public string User { get; set; }
+        public string Status { get; set; }
+        public int Steps { get; set; }
+        public int AvgSteps { get; set; }
+        public int MaxSteps { get; set; }
+        public int MinSteps { get; set; }
+
+        public static Dictionary<string, List<Users>> DicOfUser
+        {
+            get
+            {
+                return GetUsers();
+            }
+
+            set { }
+        }
+
+        
+
+        
+
+        public static Dictionary<string, List<Users>> GetUsers()
+        {
+                List<Users> list = new List<Users>();
+
+                for (int i = 1; i < 31; i++)
+                {
+                    string path = Directory.GetCurrentDirectory() + "\\TestData\\day" + i + ".json";
+
+                    var list1 = (JsonConvert.DeserializeObject<List<Users>>(File.ReadAllText(path)));
+                    list.AddRange(list1);
+                }
+
+                Dictionary<string, List<Users>> dic = new Dictionary<string, List<Users>>();
+
+                foreach (var item in list)
+                {
+                    if (!dic.ContainsKey(item.User))
+                    {
+                        dic.Add(item.User, new List<Users>());
+                    }
+                }
+                foreach (var man in list)
+                {
+                    dic[man.User].Add(man);
+                }
+
+            foreach (var item in dic)
+            {
+                var liiist = item.Value;
+                int max = 0;
+                int min = 40000;
+                int sum = 0;
+                foreach (var us in liiist)
+                {
+                    if (us.Steps > max)
+                        max = us.Steps;
+                    if (us.Steps < min)
+                        min = us.Steps;
+                    sum += us.Steps;
+                }
+                liiist[0].AvgSteps = sum / 30;
+                liiist[0].MaxSteps = max;
+                liiist[0].MinSteps = min;
+                
+            }
+                
+
+                return dic;
+        }
+
+        public static string DirGet()
+        {
+            return Directory.GetCurrentDirectory() + "\\TestData\\";
+            
+        }
+    }
+}
